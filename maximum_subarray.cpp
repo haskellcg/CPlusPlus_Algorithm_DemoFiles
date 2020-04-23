de415cf9c92ea9b08ceb951e8b5df34ba2726b7e
haskellcg <haskellcg@foxmail.com>
2020-04-23 22:44:11 +0800

    implement maximum subarray algorithm

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -42,10 +42,72 @@ bool operator==(const Maximum_SubArray_Result &oL, const Maximum_SubArray_Result
     }
 }
 
-Maximum_SubArray_Result maximum_subarray(vector<int32_t> &vecInt)
+Maximum_SubArray_Result maximum_subarray_through_mid_solver(const vector<int32_t> &vecInt,
+                                                            size_t nBegin,
+                                                            size_t nMid,
+                                                            size_t nEnd)
 {
-    // TODO: add algorithm
-    return Maximum_SubArray_Result();
+    // calculate max sum of the left side include nMid
+    int32_t nLeftSum = 0;
+    int32_t nLeftMaxSum = MIN_INT32;
+    size_t nLeftMaxIndex = nMid;
+    for (size_t i = nMid; i >= nBegin; --i){
+        nLeftSum += vecInt[i];
+        if (nLeftSum > nLeftMaxSum){
+            nLeftMaxSum = nLeftSum;
+            nLeftMaxIndex = i;
+        }
+        if (0 == i){
+            // if do --i operation, i will overflow
+            // make program endless
+            break;
+        }
+    }
+    // calcualte max sum of the right side exclude nMid
+    int32_t nRightSum = 0;
+    int32_t nRightMaxSum = MIN_INT32;
+    size_t nRightMaxIndex = nMid + 1;
+    for (size_t i = nMid + 1; i <= nEnd; ++i){
+        nRightSum += vecInt[i];
+        if (nRightSum > nRightMaxSum){
+            nRightMaxSum = nRightSum;
+            nRightMaxIndex = i;
+        }
+    }
+    return Maximum_SubArray_Result(nLeftMaxIndex, nRightMaxIndex, (nLeftMaxSum + nRightMaxSum));
+}
+
+Maximum_SubArray_Result maximum_subarray_solver(const vector<int32_t> &vecInt, size_t nBegin, size_t nEnd)
+{
+    // if only one element
+    if (nBegin == nEnd){
+        return Maximum_SubArray_Result(nBegin, nEnd, vecInt[nBegin]);
+    } else {
+        size_t nMid = (nBegin + nEnd) / 2;
+        // calculate left part
+        Maximum_SubArray_Result oMaximum_SubArray_LeftResult =
+            maximum_subarray_solver(vecInt, nBegin, nMid);
+        // calculate right part
+        Maximum_SubArray_Result oMaximum_SubArray_RightResult =
+            maximum_subarray_solver(vecInt, nMid + 1, nEnd);
+        // calculate mid part
+        Maximum_SubArray_Result oMaximum_SubArray_MidResult =
+            maximum_subarray_through_mid_solver(vecInt, nBegin, nMid, nEnd);
+        if ((oMaximum_SubArray_LeftResult.m_nSum > oMaximum_SubArray_MidResult.m_nSum) &&
+            (oMaximum_SubArray_LeftResult.m_nSum > oMaximum_SubArray_RightResult.m_nSum)){
+            return oMaximum_SubArray_LeftResult;
+        } else if ((oMaximum_SubArray_RightResult.m_nSum > oMaximum_SubArray_LeftResult.m_nSum) &&
+                   (oMaximum_SubArray_RightResult.m_nSum > oMaximum_SubArray_MidResult.m_nSum)){
+            return oMaximum_SubArray_RightResult;
+        } else {
+            return oMaximum_SubArray_MidResult;
+        }
+    }
+}
+
+Maximum_SubArray_Result maximum_subarray(const vector<int32_t> &vecInt)
+{
+    return maximum_subarray_solver(vecInt, 0, vecInt.size() - 1);
 }
 
 void maximum_subarray_test()
