f3837018c2dcb0fcbff7b2cc85805722ec883a0c
haskellcg <haskellcg@foxmail.com>
2020-04-24 17:49:28 +0800

    add strassen source file

---
+++ strassen_matrix_multiply.cpp
@@ -0,0 +1,25 @@
+/**************************************************************************
+ * author: haskellcg
+ * date: 2020-04-24
+ * description: strassen matrix multiply algorithm
+ *************************************************************************/
+
+#include "strassen_matrix_multiply.h"
+
+void strassen_matrix_multiply(const vector<vector<int32_t>> &matrixA,
+                              const vector<vector<int32_t>> &matrixB,
+                              vector<vector<int32_t>> &matrixC)
+{
+    // TODO: add strassen algorithm
+}
+
+void strassen_matrix_multiply_test()
+{
+    print_highlight_msg("Test strassen matrix multiply funtion:\n");
+    // TODO: add test code
+    if (0){
+        print_correct_msg("CORRECT\n");
+    } else {
+        print_error_msg("ERROR\n");
+    }
+}
---
+++ strassen_matrix_multiply.h
@@ -0,0 +1,33 @@
+/**************************************************************************
+ * author: haskellcg
+ * date: 2020-04-24
+ * description: strassen matrix multiply algorithm
+ *************************************************************************/
+
+#ifndef __STRASSEN_MATRIX_MULTIPLY_H__
+#define __STRASSEN_MATRIX_MULTIPLY_H__
+
+#include "common_types.h"
+
+/**
+ * @brief do matrix (n x n) multiply with strassen algorithm
+ * @param const vector<vector<int32_t>> &matrixA, matrix A
+ * @param const vector<vector<int32_t>> &matrixB, matrix B
+ * @param vector<vector<int32_t>> &matrixC, matrix C (= A x B)
+ * @return void
+ * @remarks
+ *          make sure matrix A and matrix B are all (n x n)(n rows and n columns)
+ */
+void strassen_matrix_multiply(const vector<vector<int32_t>> &matrixA,
+                              const vector<vector<int32_t>> &matrixB,
+                              vector<vector<int32_t>> &matrixC);
+
+/**
+ * @brief test strassen matrix multiply algorithm
+ * @param 
+ * @return void
+ * @remarks
+ */
+void strassen_matrix_multiply_test();
+
+#endif // __STRASSEN_MATRIX_MULTIPLY_H__
