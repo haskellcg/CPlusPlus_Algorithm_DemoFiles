ad6d56084be86fc8ff2d8b5f0030552adfc29341
haskellcg <haskellcg@foxmail.com>
2020-04-25 19:29:48 +0800

    optimize the test code in the main file

--- main.cpp
+++ main.cpp
@@ -13,23 +13,21 @@
 #include "shell_sort.h"
 #include "strassen_matrix_multiply.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    bubble_sort_test();
-
-    heap_sort_test();
-
-    insert_sort_test();
-
-    maximum_subarray_test();
-
-    merge_sort_test();
-
-    selection_sort_test();
-
-    shell_sort_test();
-    
-    strassen_matrix_multiply_test();
+    if ((argc >= 2) && ("-a" == string(argv[1]))){
+        bubble_sort_test();
+        heap_sort_test();
+        insert_sort_test();
+        maximum_subarray_test();
+        merge_sort_test();
+        selection_sort_test();
+        shell_sort_test();
+        strassen_matrix_multiply_test();
+    } else {
+        // default only test the current coding algorithm
+        heap_sort_test();
+    }
 
     return 0;
 }
