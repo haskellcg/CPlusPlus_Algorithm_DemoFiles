05ba21ff800a34cd91c641009db9829b308b7608
haskellcg <haskellcg@foxmail.com>
2020-10-10 13:57:57 +0800

    add basic stack interfaces and tests

--- basic_stack.cpp
+++ basic_stack.cpp
@@ -6,7 +6,91 @@
 
 #include "basic_stack.h"
 
+basic_stack::basic_stack()
+{
+    m_nCapacity = 1;
+    m_nSize = 0;
+    m_pData = new int32_t[m_nCapacity];
+}
+
+basic_stack::basic_stack(const basic_stack &oStack)
+{
+    // TODO
+}
+
+basic_stack &basic_stack::operator=(const basic_stack &oStack)
+{
+    // TODO
+    return *this;
+}
+
+basic_stack::~basic_stack()
+{
+    if (NULL != m_pData){
+        delete []m_pData;
+    }
+    m_nSize = 0;
+    m_nCapacity = 0;
+}
+
+size_t basic_stack::size() const
+{
+    return m_nSize;
+}
+
+string basic_stack::to_string() const
+{
+    // TODO
+    vector<int32_t> vecInt;
+    for (size_t i = 0; i < m_nSize; ++i){
+        vecInt.push_back(*(m_pData + i));
+    }
+    return ::to_string(vecInt);
+}
+
+void basic_stack::push(int32_t nInt)
+{
+    // TODO
+}
+
+bool basic_stack::pop(int32_t &nInt)
+{
+    // TODO
+    return true;
+}
+
+void basic_stack::reserve(size_t nCapacity)
+{
+    // TODO
+}
+
 void basic_stack_test()
 {
     print_highlight_msg(">>> Test basic stack straucture:\n");
+    int32_t arrayInt[10] = {76, 28, -34, -56, 89, 92, 66, 49, 15, 7};
+    basic_stack oStack;
+    for (size_t i = 0; i < 10; ++i){
+        oStack.push(arrayInt[i]);
+    }
+    if (10 != oStack.size()){
+        print_error_msg("basic stack size error\n");
+    } else {
+        print_correct_msg("basic stack size correct\n");
+    }
+    basic_stack oStackCopy;
+    oStackCopy = oStack;
+    if (10 != oStackCopy.size()){
+        print_error_msg("basic stack copy size error\n");
+    } else {
+        print_correct_msg("basic stack copy size correct\n");
+    }
+    int32_t nTop = MIN_INT32;
+    oStack.pop(nTop);
+    oStack.pop(nTop);
+    oStack.pop(nTop);
+    if (arrayInt[7] != nTop){
+        print_error_msg("basic stack pop error\n");
+    } else {
+        print_correct_msg("basic stack pop correct\n");
+    }
 }
--- basic_stack.h
+++ basic_stack.h
@@ -10,6 +10,94 @@
 #include "common_types.h"
 
 /**
+ * @brief basic stack data structure
+ */
+class basic_stack
+{
+public:
+    /**
+     * @brief constructor
+     * @param
+     * @return
+     * @remarks
+     */
+    basic_stack();
+
+    /**
+     * @brief copy constructor
+     * @param const basic_stack &oStack, other stack
+     * @return
+     * @remarks
+     */
+    basic_stack(const basic_stack &oStack);
+
+    /**
+     * @brief override operator =
+     * @param const basic_stack &oStack, other stack
+     * @return basic_stack &, *this
+     * @remarks
+     */
+    basic_stack &operator=(const basic_stack &oStack);
+
+    /**
+     * @brief destructor
+     * @param
+     * @return
+     * @remarks
+     */
+    virtual ~basic_stack();
+    
+    /**
+     * @brief get the size of the stack
+     * @param
+     * @return size_t, number of element in the stack
+     * @remarks
+     */
+    size_t size() const;
+
+    /**
+     * @brief print detail information
+     * @param
+     * @return string
+     * @remarks
+     */
+    string to_string() const;
+
+    /**
+     * @brief push nInt into stack
+     * @param int32_t nInt,
+     * @return void
+     * @remarks
+     */
+    void push(int32_t nInt);
+
+    /**
+     * @brief get the top element of stack and remove it from stack
+     * @param int32_t &nInt, top element
+     * @return bool, false if no element left
+     * @remarks
+     */
+    bool pop(int32_t &nInt);
+
+private:
+    /**
+     * @brief change the capacity of the stack
+     * @param size_t nCapacity, new capacity
+     * @return void
+     * @remarks
+     */
+    void reserve(size_t nCapacity);
+
+private:
+    /*< data of stack */
+    int32_t *m_pData;
+    /*< number of element in the stack */
+    size_t m_nSize;
+    /*< total capacity */
+    size_t m_nCapacity;
+};
+
+/**
  * @brief test data structure stack
  * @param
  * @return void
