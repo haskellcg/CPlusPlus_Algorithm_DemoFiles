59d55c8030c6c602501c00ae4074841def1ef58f
haskellcg <haskellcg@foxmail.com>
2020-10-16 19:38:38 +0800

    implement basic stack

--- basic_stack.cpp
+++ basic_stack.cpp
@@ -15,12 +15,26 @@ basic_stack::basic_stack()
 
 basic_stack::basic_stack(const basic_stack &oStack)
 {
-    // TODO
+    m_nCapacity = oStack.m_nCapacity;
+    m_nSize = oStack.m_nSize;
+    m_pData = new int32_t[m_nCapacity];
+    for (size_t i = 0; i < m_nSize; ++i){
+        *(m_pData + i) = *(oStack.m_pData + i);
+    }
 }
 
 basic_stack &basic_stack::operator=(const basic_stack &oStack)
 {
-    // TODO
+    if (&oStack == this){
+        return *this;
+    }
+    delete []m_pData;
+    m_nCapacity = oStack.m_nCapacity;
+    m_nSize = oStack.m_nSize;
+    m_pData = new int32_t[m_nCapacity];
+    for (size_t i = 0; i < m_nSize; ++i){
+        *(m_pData + i) = *(oStack.m_pData + i);
+    }
     return *this;
 }
 
@@ -40,28 +54,48 @@ size_t basic_stack::size() const
 
 string basic_stack::to_string() const
 {
-    // TODO
+    ostringstream oss;
     vector<int32_t> vecInt;
     for (size_t i = 0; i < m_nSize; ++i){
         vecInt.push_back(*(m_pData + i));
     }
-    return ::to_string(vecInt);
+    oss << "Data:" << ::to_string(vecInt) << "\n";
+    oss << "Size:" << m_nSize << "\n";
+    oss << "Capacity:" << m_nCapacity;
+    return oss.str();
 }
 
 void basic_stack::push(int32_t nInt)
 {
-    // TODO
+    if (m_nSize >= m_nCapacity){
+        reserve(m_nCapacity << 1);
+    }
+    *(m_pData + m_nSize) = nInt;
+    ++m_nSize;
 }
 
 bool basic_stack::pop(int32_t &nInt)
 {
-    // TODO
-    return true;
+    if (m_nSize > 0){
+        nInt = *(m_pData + m_nSize - 1);
+        --m_nSize;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 void basic_stack::reserve(size_t nCapacity)
 {
-    // TODO
+    if (nCapacity > m_nCapacity){
+        int32_t *pNewData = new int32_t[nCapacity];
+        for (size_t i = 0; i < m_nSize; ++i){
+            *(pNewData + i) = *(m_pData + i);
+        }
+        delete []m_pData;
+        m_pData = pNewData;
+        m_nCapacity = nCapacity;
+    }
 }
 
 void basic_stack_test()
@@ -71,6 +105,7 @@ void basic_stack_test()
     basic_stack oStack;
     for (size_t i = 0; i < 10; ++i){
         oStack.push(arrayInt[i]);
+        // print_warning_msg(oStack.to_string() + "\n");
     }
     if (10 != oStack.size()){
         print_error_msg("basic stack size error\n");
