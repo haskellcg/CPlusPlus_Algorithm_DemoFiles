046850ab041fba451f8325dd6a1a585638b4e7af
haskellcg <haskellcg@foxmail.com>
2020-10-28 16:19:03 +0800

    add nowcoder

---
+++ nowcoder_com_first_part.cpp
@@ -0,0 +1,78 @@
+/**************************************************************************
+ * author: haskellcg
+ * date: 2020-10-28
+ * description: nowcoder.com practice codes
+ *************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "nowcoder_com_first_part.h"
+
+using namespace std;
+
+void nowcoder_com_hw_hj1()
+{
+    char cBuff[5000];
+
+    // cin.get(cBuff, 5000);
+    cin.getline(cBuff, 5000);
+    int nIndex = 0;
+    int nWordLength = 0;
+    while (true){
+        if (('\0' != cBuff[nIndex]) && (' ' != cBuff[nIndex])){
+            ++nWordLength;
+        } else if ('\0' == cBuff[nIndex]){
+            break;
+        } else {
+            nWordLength = 0;
+        }
+        ++nIndex;
+    }
+    cout << nWordLength;
+
+    /*
+     * 方法二
+    // scanf stops when met space or enter, so cBuff will store the last word
+    // EOF == -1, so ~EOF == 0
+    // Ctrl + D in linux is EOF
+    while (EOF != scanf("%s", cBuff));
+    printf("%lu", strlen(cBuff));
+    */
+}
+
+void nowcoder_com_hw_hj2()
+{
+    char cBuff[1024];
+    char cCharacter;
+    cin.getline(cBuff, 1024);
+    cin >> cCharacter;
+    for (int i = 0; i < 1024; ++i){
+        if ('\0' == cBuff[i]){
+            break;
+        }
+        if ((cBuff[i] >= 'A') && (cBuff[i]) <= 'Z'){
+            cBuff[i] += 'a' - 'A';
+        }
+    }
+    if ((cCharacter >= 'A') && (cCharacter <= 'Z')){
+        cCharacter += 'a' - 'A';
+    }
+    int nSameCharNumber = 0;
+    for (int i = 0; i < 1024; ++i){
+        if ('\0' == cBuff[i]){
+            break;
+        }
+        if (cBuff[i] == cCharacter){
+            ++nSameCharNumber;
+        }
+    }
+    cout << nSameCharNumber;
+}
+
+void nowcoder_com_test()
+{
+    // nowcoder_com_hw_hj1();
+    // nowcoder_com_hw_hj2();
+}
---
+++ nowcoder_com_first_part.h
@@ -0,0 +1,52 @@
+/**************************************************************************
+ * author: haskellcg
+ * date: 2020-10-28
+ * description: nowcoder.com practice codes
+ *************************************************************************/
+
+#ifndef __NOWCODER_COM_FIRST_PART_H__
+#define __NOWCODER_COM_FIRST_PART_H__
+
+/**************************************************************************
+ *
+ * https://www.nowcoder.com/ta/huawei
+ *
+ *************************************************************************/
+
+/**
+ * @brief 
+        题目描述
+        计算字符串最后一个单词的长度，单词以空格隔开。
+        输入描述:
+        输入一行，代表要计算的字符串，非空，长度小于5000。
+        输出描述:
+        输出一个整数，表示输入字符串最后一个单词的长度。
+ * @param
+ * @return void
+ * @remarks
+ */
+void nowcoder_com_hw_hj1();
+
+/**
+ * @brief
+        题目描述
+        写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写。
+        输入描述:
+        第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+        输出描述:
+        输出输入字符串中含有该字符的个数。
+ * @param
+ * @return void
+ * @remarks
+ */
+void nowcoder_com_hw_hj2();
+
+/**
+ * @brief test all the test functions
+ * @param
+ * @return void
+ * @remarks
+ */
+void nowcoder_com_test();
+
+#endif // __NOWCODER_COM_FIRST_PART_H__
