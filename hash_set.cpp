0f93e3e57eef32415f5731d22f92ca13795ef5d6
haskellcg <haskellcg@foxmail.com>
2020-11-15 19:08:18 +0800

    implement open addressing hash set

--- hash_set.cpp
+++ hash_set.cpp
@@ -76,6 +76,109 @@ bool Chaining_Hash_Set::remove(uint32_t nKey)
     }
 }
 
+OpenAddress_Hash_Set::OpenAddress_Hash_Set()
+:   m_nCapacity(1 << CAPACITY_OF_HASHSET_LOG2),
+    m_nSize(0),
+    m_pData(NULL)
+{
+    m_pData = new uint32_t[m_nCapacity];
+    for (size_t i = 0; i < m_nCapacity; ++i){
+        *(m_pData + i) = OPENADDRESS_INVALID_DATA;
+    }
+}
+
+OpenAddress_Hash_Set::~OpenAddress_Hash_Set()
+{
+    m_nCapacity = 0;
+    m_nSize = 0;
+    if (NULL != m_pData){
+        delete []m_pData;
+        m_pData = NULL;
+    }
+}
+
+string OpenAddress_Hash_Set::to_string() const
+{
+    ostringstream ossResult;
+    ossResult << "capacity:" << m_nCapacity << "\n";
+    ossResult << "size:" << m_nSize << "\n";
+    ossResult << "data:[";
+    for (size_t i = 0; i < m_nCapacity; ++i){
+        if (0 != i){
+            ossResult << ", ";
+        }
+        if (OPENADDRESS_INVALID_DATA == *(m_pData + i)){
+            ossResult << "INVALID";
+        } else if (OPENADDRESS_DELETED_DATA == *(m_pData + i)){
+            ossResult << "DELETED";
+        } else {
+            ossResult << *(m_pData + i);
+        }
+    }
+    ossResult << "]";
+    return ossResult.str();
+}
+
+size_t OpenAddress_Hash_Set::size() const
+{
+    return m_nSize;
+}
+
+bool OpenAddress_Hash_Set::search(uint32_t nKey, size_t &nIndex) const
+{
+    size_t nProbTimes = 0;
+    while (nProbTimes < m_nCapacity){
+        size_t nProbIndex = probing(nKey, nProbTimes);
+        if (nKey == *(m_pData + nProbIndex)){
+            nIndex = nProbIndex;
+            return true;
+        } else if (OPENADDRESS_INVALID_DATA == *(m_pData + nProbIndex)){
+            break;
+        }
+        ++nProbTimes;
+    }
+    return false;
+}
+
+bool OpenAddress_Hash_Set::insert(uint32_t nKey)
+{
+    size_t nProbTimes = 0;
+    while (nProbTimes < m_nCapacity){
+        size_t nProbIndex = probing(nKey, nProbTimes);
+        if ((OPENADDRESS_INVALID_DATA == *(m_pData + nProbIndex))
+            || (OPENADDRESS_DELETED_DATA == *(m_pData + nProbIndex))){
+            *(m_pData + nProbIndex) = nKey;
+            ++m_nSize;
+            return true;
+        }
+        ++nProbTimes;
+    }
+    return false;
+}
+
+bool OpenAddress_Hash_Set::remove(uint32_t nKey)
+{
+    size_t nProbTimes = 0;
+    while (nProbTimes < m_nCapacity){
+        size_t nProbIndex = probing(nKey, nProbTimes);
+        if (nKey == *(m_pData + nProbIndex)){
+            *(m_pData + nProbIndex) = OPENADDRESS_DELETED_DATA;
+            --m_nSize;
+            return true;
+        } else if (OPENADDRESS_INVALID_DATA == *(m_pData + nProbIndex)){
+            break;
+        }
+        ++nProbTimes;
+    }
+    return false;
+}
+
+size_t OpenAddress_Hash_Set::probing(uint32_t nKey, size_t nProbTimes) const
+{
+    size_t nGK = division_hash(nKey, m_nCapacity);
+    return ((nGK + (nProbTimes * multiplication_hash(nKey, CAPACITY_OF_HASHSET_LOG2))) % m_nCapacity);
+}
+
 void hash_set_test()
 {
     print_highlight_msg(">>> Test hash set:\n");
@@ -86,7 +189,7 @@ void hash_set_test()
     }
 
     vector<uint32_t> vecInt;
-    get_random_vector(vecInt, 1 << (CAPACITY_OF_HASHSET_LOG2 + 1));
+    get_random_vector(vecInt, 1 << (CAPACITY_OF_HASHSET_LOG2 + 0));
     print_highlight_msg("data to be inserted:\n");
     print_normal_msg(to_string(vecInt) + "\n");
 
@@ -110,5 +213,23 @@ void hash_set_test()
     }
     print_normal_msg(oChainingHashSet.to_string() + "\n");
 
-    print_error_msg("no test case yet\n");
+    OpenAddress_Hash_Set oOpenAddressHashSet;
+
+    print_highlight_msg("open addressing hash set state when inserting:\n");
+
+    for (size_t i = 0; i < vecInt.size(); ++i){
+        if (!oOpenAddressHashSet.insert(vecInt[i])){
+            print_warning_msg("data contains same key\n");
+        }
+    }
+    print_normal_msg(oOpenAddressHashSet.to_string() + "\n");
+
+    print_highlight_msg("open addressing hash set state when deleteing:\n");
+
+    for (size_t i = 0; i < vecInt.size(); ++i){
+        if (!oOpenAddressHashSet.remove(vecInt[i])){
+            print_warning_msg("can't find key\n");
+        }
+    }
+    print_normal_msg(oOpenAddressHashSet.to_string() + "\n");
 }
--- hash_set.h
+++ hash_set.h
@@ -8,6 +8,11 @@
 #include "double_link_list.h"
 
 #define CAPACITY_OF_HASHSET_LOG2 4
+#define OPENADDRESS_INVALID_DATA MAX_UINT32
+
+// DELETED is not same with INVALID, if not, 
+// data inserted can't get found if it probing through DELETED data
+#define OPENADDRESS_DELETED_DATA (MAX_UINT32 - 1)
 
 /**
  * @brief hash set with chaining to deal with collision
@@ -79,6 +84,101 @@ private:
 private:
     /*< use double linked list as chaining */
     Double_Link_List *m_arrayDDL[1 << CAPACITY_OF_HASHSET_LOG2];
+};
+
+/**
+ * @brief use open addressing to deal with collision
+ */
+class OpenAddress_Hash_Set
+{
+public:
+    /**
+     * @brief default constructor
+     */
+    OpenAddress_Hash_Set();
+
+    /**
+     * @brief destructor
+     */
+    ~OpenAddress_Hash_Set();
+
+    /**
+     * @brief print member information
+     * @param
+     * @return string, information
+     * @remarks
+     */
+    string to_string() const;
+
+    /**
+     * @brief number of valid elements in the hash set
+     * @param
+     * @return size
+     * @remarks
+     */
+    size_t size() const;
+
+    /**
+     * @brief search key in the hash set
+     * @param uint32_t nKey, key
+     * @param size_t &nIndex, index of the key if key is in hash set
+     * @return bool, true if key is in hash set, false if not
+     * @remarks
+     */
+    bool search(uint32_t nKey, size_t &nIndex) const;
+
+    /**
+     * @brief insert key to the hash set
+     * @param uint32_t nKey, key
+     * @return bool, true if inserted, false if not
+     * @remarks
+     */
+    bool insert(uint32_t nKey);
+
+    /**
+     * @brief remove nKey from hash set
+     * @param uint32_t nKey, key
+     * @return bool, true if deleted, false if key not in the hash set
+     * @remarks
+     */
+    bool remove(uint32_t nKey);
+
+private:
+    /**
+     * @brief return the index need to be prob
+     * @param uint32_t nKey, key
+     * @param size_t nProbTimes, probe times
+     * @return size_t, index of next probing
+     * @remarks
+     *          m -- capacity of the hash set
+     *          i -- times of probing
+     *          linear probing: 
+     *                          h(k, i) = (h'(k) + i) mod m
+     *          quadratic probing: 
+     *                          h(k, i) = (h'(k) + (a * i) + (b * i ^ 2)) mod m
+     *          double probing:
+     *                          h(k, i) = (g(k) + i * f(k)) mod m
+     *          we use double probing here
+     */
+    size_t probing(uint32_t nKey, size_t nProbTimes) const;
+
+    /**
+     * @brief private copy constructor
+     */
+    OpenAddress_Hash_Set(const OpenAddress_Hash_Set &oOtherOpenAddressHashSet);
+
+    /**
+     * @brief private assign funtion
+     */
+    OpenAddress_Hash_Set &operator=(const OpenAddress_Hash_Set &oOtherOpenAddressHashSet);
+
+private:
+    /*< capacity of the hash set */
+    size_t m_nCapacity;
+    /*< number of element in the hash set */
+    size_t m_nSize;
+    /*< store data */
+    uint32_t *m_pData;
 };
 
 /**
