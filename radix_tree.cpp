08a1db96d29d4e3959e062473e1c8895956a4cc9
haskellcg <haskellcg@foxmail.com>
2020-12-09 22:24:30 +0800

    add part of node type for radix tree

--- radix_tree.cpp
+++ radix_tree.cpp
@@ -6,6 +6,41 @@
 
 #include "radix_tree.h"
 
+string RTBaseNode::get_value() const
+{
+    return m_strValue;
+}
+
+void RTBaseNode::set_value(const string &strValue)
+{
+    m_strValue = strValue;
+}
+
+RTNodeType RTInnerNode4::get_node_type() const
+{
+    return RTNodeType::INNER_NODE_4;
+}
+
+RTNodeType RTInnerNode16::get_node_type() const
+{
+    return RTNodeType::INNER_NODE_16;
+}
+
+RTNodeType RTInnerNode48::get_node_type() const
+{
+    return RTNodeType::INNER_NODE_48;
+}
+
+RTNodeType RTInnerNode256::get_node_type() const
+{
+    return RTNodeType::INNER_NODE_256;
+}
+
+RTNodeType RTLeafNode::get_node_type() const
+{
+    return RTNodeType::LEAF_NODE;
+}
+
 void radix_tree_test()
 {
     print_highlight_msg(">>> Test radix tree:\n");
--- radix_tree.h
+++ radix_tree.h
@@ -10,17 +10,151 @@
 #include "common_types.h"
 
 /**
+ * @brief radix tree node types
+ */
+enum RTNodeType
+{
+    INNER_NODE_4,
+    INNER_NODE_16,
+    INNER_NODE_48,
+    INNER_NODE_256,
+    LEAF_NODE
+};
+
+/**
+ * @brief base radix tree node
+ */
+class RTBaseNode
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const = 0;
+    
+    /**
+     * @brief get value
+     */
+    string get_value() const;
+
+    /**
+     * @brief set value
+     */
+    void set_value(const string &strValue);
+
+protected:
+    /*< the value of the node */
+    string m_strValue;
+};
+
+/**
+ * @brief inner node 4
+ */
+class RTInnerNode4
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const;
+};
+
+/**
+ * @brief inner node 16
+ */
+class RTInnerNode16
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const;
+};
+
+/**
+ * @brief inner node 48
+ */
+class RTInnerNode48
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const;
+};
+
+/**
+ * @brief inner node 4
+ */
+class RTInnerNode256
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const;
+};
+
+/**
+ * @brief leaf node
+ */
+class RTLeafNode
+{
+public:
+    /**
+     * @brief get node type
+     * @param
+     * @return RTNodeType, node type enum
+     * @remarks
+     */
+    virtual RTNodeType get_node_type() const;
+};
+
+/**
  * @brief radix tree
  * @remarks
  *          IPs problem: https://blog.sqreen.com/demystifying-radix-trees/
- *          Visualization: https://www.cs.usfca.edu/~galles/visualization/RadixTree.html
  *          Bloom Filter: https://en.wikipedia.org/wiki/Bloom_filter
+ *
+ *          Visualization: https://www.cs.usfca.edu/~galles/visualization/RadixTree.html
  *          Assocaitive Array: https://en.wikipedia.org/wiki/Associative_array
  *          Radix Tree: https://en.wikipedia.org/wiki/Radix_tree
  *          Difference Radix Tree with Trie: https://stackoverflow.com/questions/14708134/
  *                                           what-is-the-difference-between-trie-and-radix-trie-data-structures
  *          Radix Tree in Linux: https://lwn.net/Articles/175432/
  *          Radix Tree Adaptive: https://db.in.tum.de/~leis/papers/ART.pdf
+ *                               T tree
+ *                               B+ tree
+ *                               CSB+ tree
+ *                               k-ary search tree
+ *                               FAST-the fast architecture sensitive tree
+ *                               ART-adaptive radix tree: path compression, lazy expansion
+ *                                                        Space usage can be excessive when most child pointers are null
+ *                                                        GTP use span 4 bits; linux kernel use span 6 bits
+ *                                                        binary comparable keys(difination/transformations)
+ *                               burst trie
+ *                               HAT-trie
+ *                               the Generalized Prefix tree
+ *                               KISS tree
+ *          Cache: https://www.geeksforgeeks.org/cache-memory-in-computer-organization/
+ *                 https://www.geeksforgeeks.org/cache-organization-set-1-introduction/?ref=lbp
+ *                 https://www.geeksforgeeks.org/whats-difference-between-cpu-cache-and-tlb/?ref=lbp
+ *          Reiser4 FS: https://web.archive.org/web/20071024001500/http://www.namesys.com/v4/v4.html#soft_eng
  */
 class Radix_Tree
 {
