cc31e22a13b2393aefa27eca3666cf0557471273
haskellcg <haskellcg@foxmail.com>
2020-12-27 17:26:51 +0800

    add RBTNode and declaration for Red_Black_Tree class

--- red_black_tree.cpp
+++ red_black_tree.cpp
@@ -6,8 +6,96 @@
 
 #include "red_black_tree.h"
 
+RBTNode::RBTNode()
+:   m_enumColor(RBTNODE_BLACK),
+    m_nKey(MAX_UINT32),
+    m_pParent(NULL),
+    m_pLeft(NULL),
+    m_pRight(NULL)
+{
+}
+
+RBTNode::~RBTNode()
+{
+    m_enumColor = RBTNODE_BLACK;
+    m_nKey = MAX_UINT32;
+    m_pParent = NULL;
+    m_pLeft = NULL;
+    m_pRight = NULL;
+}
+
+string RBTNode::to_string() const
+{
+    ostringstream ossResult;
+    ossResult << m_nKey << ":"
+              << ((RBTNODE_RED == m_enumColor) ? "R" : "B");
+    return ossResult.str();
+}
+
+RBTNode_Color RBTNode::get_color() const
+{
+    return m_enumColor;
+}
+
+void RBTNode::set_color(RBTNode_Color enumColor)
+{
+    m_enumColor = enumColor;
+}
+
+uint32_t RBTNode::get_key() const
+{
+    return m_nKey;
+}
+
+void RBTNode::set_key(uint32_t nKey)
+{
+    m_nKey = nKey;
+}
+
+RBTNode *RBTNode::get_parent() const
+{
+    return m_pParent;
+}
+
+void RBTNode::set_parent(RBTNode *pParent)
+{
+    m_pParent = pParent;
+}
+
+RBTNode *RBTNode::get_left() const
+{
+    return m_pLeft;
+}
+
+void RBTNode::set_left(RBTNode *pLeft)
+{
+    m_pLeft = pLeft;
+}
+
+RBTNode *RBTNode::get_right() const
+{
+    return m_pRight;
+}
+
+void RBTNode::set_right(RBTNode *pRight)
+{
+    m_pRight = pRight;
+}
+
 void red_black_tree_test()
 {
     print_highlight_msg(">>> Test red balck tree:\n");
+
+    uint32_t arrayInt[] = {10, 11, 12, 13, 14, 15, 5, 4, 3, 2, 1, 9, 8, 7, 6};
+    vector<uint32_t> vecInt;
+    vecInt.insert(vecInt.end(), begin(arrayInt), end(arrayInt));
+    print_normal_msg(to_string(vecInt) + "\n");
+
+    Red_Black_Tree oRedBlackTree;
+    for (size_t i = 0; i < vecInt.size(); ++i){
+        oRedBlackTree.insert(vecInt[i]);
+        print_warning_msg(oRedBlackTree.to_string() + "\n");
+    }
+        
     print_error_msg("No test case yet.\n");
 }
--- red_black_tree.h
+++ red_black_tree.h
@@ -11,6 +11,109 @@
 #include "common_types.h"
 
 /**
+ * @brief Red_Black_Tree Node color
+ */
+enum RBTNode_Color
+{
+    RBTNODE_RED,
+    RBTNODE_BLACK
+};
+
+/**
+ * @brief Red_Black_Tree Node
+ */
+class RBTNode
+{
+public:
+    /**
+     * @brief default constructor
+     */
+    RBTNode();
+
+    /**
+     * @brief destructor
+     */
+    ~RBTNode();
+    
+    /**
+     * @brief to string
+     * @param
+     * @return string
+     * @remarks
+     *          in order to show node in the tree perfectly,
+     *          node should only show important information,
+     *          and should be less then 6 characters
+     */
+    string to_string() const;
+
+    /**
+     * @brief get node color
+     */
+    RBTNode_Color get_color() const;
+
+    /**
+     * @brief set node color
+     */
+    void set_color(RBTNode_Color enumColor);
+
+    /**
+     * @brief get key
+     */
+    uint32_t get_key() const;
+
+    /**
+     * @brief set key
+     */
+    void set_key(uint32_t nKey);
+
+    /**
+     * @brief get parent node link
+     */
+    RBTNode *get_parent() const;
+
+    /**
+     * @brief set parent node link
+     */
+    void set_parent(RBTNode *pParent);
+
+    /**
+     * @brief get left child node link
+     */
+    RBTNode *get_left() const;
+
+    /**
+     * @brief set left child node link
+     */
+    void set_left(RBTNode *pLeft);
+
+    /**
+     * @brief get right child node link
+     */
+    RBTNode *get_right() const;
+
+    /**
+     * @brief set right child node link
+     */
+    void set_right(RBTNode *pRight);
+
+private:
+    /*< color of the node */
+    RBTNode_Color m_enumColor;
+    /*< key of the node */
+    uint32_t m_nKey;
+    /*< link to parent node */
+    RBTNode *m_pParent;
+    /*< link to left child */
+    RBTNode *m_pLeft;
+    /*< link to right child */
+    RBTNode *m_pRight;
+};
+
+class Red_Black_Tree: public Binary_Search_Tree<RBTNode>
+{
+};
+
+/**
  * @brief test red black tree data structure
  * @param
  * @return void
