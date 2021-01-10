de0acd615618496ee381e72df95b75b45f6218a2
Haskellcg <haskellcg@foxmail.com>
2021-01-10 19:15:28 +0800

    add related links for strassen matrix multiply

--- strassen_matrix_multiply.h
+++ strassen_matrix_multiply.h
@@ -18,6 +18,9 @@
  * @remarks
  *          make sure matrix A and matrix B are all (n x n)(n rows and n columns)
  *          and n is the power of 2
+ *
+ *          Coppersmith-Winograd algorithm: https://en.wikipedia.org/wiki/Coppersmith%E2%80%93Winograd_algorithm    
+ *          Galactic algorithm: https://en.wikipedia.org/wiki/Galactic_algorithm
  */
 void strassen_matrix_multiply(const vector<vector<int32_t>> &matrixA,
                               const vector<vector<int32_t>> &matrixB,
