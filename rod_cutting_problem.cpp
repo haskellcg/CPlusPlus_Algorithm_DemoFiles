ebf69ecd21833eb38da7925cd633b7c64bf54ee9
haskellcg <haskellcg@foxmail.com>
2021-01-12 22:58:11 +0800

    add test cases and rod cutting problem solution

--- rod_cutting_problem.cpp
+++ rod_cutting_problem.cpp
@@ -9,5 +9,28 @@
 void rod_cutting_problem_solution_test()
 {
     print_highlight_msg(">>> Test rod cutting problem solution:\n");
+    map<uint32_t, uint32_t> mapPriceTable;
+    mapPriceTable.insert(make_pair(1, 1));
+    mapPriceTable.insert(make_pair(2, 5));
+    mapPriceTable.insert(make_pair(3, 8));
+    mapPriceTable.insert(make_pair(4, 9));
+    mapPriceTable.insert(make_pair(5, 10));
+    mapPriceTable.insert(make_pair(6, 17));
+    mapPriceTable.insert(make_pair(7, 17));
+    mapPriceTable.insert(make_pair(8, 20));
+    mapPriceTable.insert(make_pair(9, 24));
+    mapPriceTable.insert(make_pair(10, 30));
+
+    Rod_Cutting_Problem_Solution oRodCuttingProblemSolution;
+    oRodCuttingProblemSolution.set_price_table(mapPriceTable);
+
+    oRodCuttingProblemSolution.top_down_memoization_method(30);
+    print_normal_msg("top down memoization method result:\n");
+    print_normal_msg(oRodCuttingProblemSolution.show_solution());
+
+    oRodCuttingProblemSolution.bottom_up_method(30);
+    print_normal_msg("bottom up method result:\n");
+    print_normal_msg(oRodCuttingProblemSolution.show_solution());
+
     PRINT_DEFAULT_ERROR
 }
--- rod_cutting_problem.h
+++ rod_cutting_problem.h
@@ -7,7 +7,62 @@
 #ifndef __ROD_CUTTING_PROBLEM_H__
 #define __ROD_CUTTING_PROBLEM_H__
 
+#include <map>
 #include "common_types.h"
+
+/**
+ * @brief solution for rod cutting problem
+ */
+class Rod_Cutting_Problem_Solution
+{
+public:
+    /**
+     * @brief default constructor
+     */
+    Rod_Cutting_Problem_Solution();
+
+    /**
+     * @brief print cut solution given certain length of rod
+     * @param
+     * @return string
+     * @remarks
+     */
+    string show_solution() const;
+
+    /**
+     * @brief set price table for current solution
+     * @param const map<uint32_t, uint32_t> &mapPriceTable, price table
+     * @return void
+     * @remarks
+     */
+    void set_price_table(const map<uint32_t, uint32_t> &mapPriceTable);
+
+    /**
+     * @brief find solution with top-down memoization method
+     * @param uint32_t nLength, length of the rod
+     * @return void
+     * @remarks
+     */
+    void top_down_memoization_method(uint32_t nLength);
+
+    /**
+     * @brief find solution with down-up method
+     * @param uint32_t nLength, length of the rod
+     * @return void
+     * @remarks
+     */
+    void bottom_up_method(uint32_t nLength);
+
+private:
+    /*< rod price table, <rod length, price> */
+    map<uint32_t, uint32_t> m_mapPriceTable;
+    /*< optimal solution for certain length */
+    vector<uint32_t> m_vecCutSolution;
+    /*< top-down memoization result table */
+    vector<uint32_t> m_vecTopDownTable;
+    /*< bottom-up result table */
+    vector<uint32_t> m_vecBottomUpTable;
+};
 
 /**
  * @brief test rod cutting problem solution 
