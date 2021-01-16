8b504bf7928a9f2c1f370f1b8f9d209e3c6e605b
haskellcg <haskellcg@foxmail.com>
2021-01-16 16:20:26 +0800

    add matrix chain multiplication related topics

--- matrix_chain_multiplication_problem.h
+++ matrix_chain_multiplication_problem.h
@@ -10,6 +10,25 @@
 #include "common_types.h"
 
 /**
+ * @brief topics about matrix chain multiplication
+ *          words:
+ *                  vertex      => vertices
+ *                  matrix      => matrices
+ *                  hexagon     => https://en.wikipedia.org/wiki/Hexagon
+ *
+ *          matrix chain multiplication wiki:                 https://en.wikipedia.org/wiki/Matrix_chain_multiplication
+ *          matrix chain multiplication youtube introduction: https://www.youtube.com/watch?v=prx1psByp7U
+ *
+ *          catalan number:       https://mathworld.wolfram.com/CatalanNumber.html
+ *          catalan number:       https://en.wikipedia.org/wiki/Catalan_number
+ *
+ *          Chin-Hu-Shing Approximating Algorithm:
+ *                                https://pdfs.semanticscholar.org/e8b8/40a921f7967b30ac161b3dd9654b27998ddb.pdf
+ *          Hu & Shing:           http://www.cs.ust.hk/mjg_lib/bibs/DPSu/DPSu.Files/0211028.pdf
+ *                                http://www.cs.ust.hk/mjg_lib/bibs/DPSu/DPSu.Files/0213017.pdf
+ */
+
+/**
  * @brief test matrix chain multiplication problem solution
  * @param
  * @return void
