f7e220c3a9e0982e38a2e972690361857ce0a127
haskellcg <haskellcg@foxmail.com>
2021-01-17 11:58:17 +0800

    add implementation for matrix chain multiply problem

--- matrix_chain_multiplication_problem.cpp
+++ matrix_chain_multiplication_problem.cpp
@@ -6,8 +6,74 @@
 
 #include "matrix_chain_multiplication_problem.h"
 
+Matrix_Chain_Multiplication_Problem_Solution::Matrix_Chain_Multiplication_Problem_Solution()
+{
+}
+
+void Matrix_Chain_Multiplication_Problem_Solution::set_matrices(vector<pair<uint32_t, uint32_t>> &vecMatrices)
+{
+    m_vecMatrices = vecMatrices;
+}
+
+void Matrix_Chain_Multiplication_Problem_Solution::dp_bottom_up_method(vector<vector<uint32_t>> &matrixMultiplyTimes, vector<vector<uint32_t>> &matrixBracketPosition)
+{
+    for (size_t i = 0; i < m_vecMatrices.size(); ++i){
+        vector<uint32_t> vecRow;
+        for (size_t j = 0; j < m_vecMatrices.size(); ++j){
+            vecRow.push_back(MAX_UINT32);
+        }
+        matrixMultiplyTimes.push_back(vecRow);
+        matrixBracketPosition.push_back(vecRow);
+    }
+
+    for (size_t i = 0; i < m_vecMatrices.size(); ++i){
+        matrixMultiplyTimes[i][i] = 0;
+    }
+
+    for (size_t nSubProblemSize = 1; nSubProblemSize < m_vecMatrices.size(); ++nSubProblemSize){
+        for (size_t nStartIndex = 0; nStartIndex < (m_vecMatrices.size() - nSubProblemSize); ++nStartIndex){
+            size_t nEndIndex = nStartIndex + nSubProblemSize;
+            // subproblem [nStartIndex, nEndIndex]
+            uint32_t nMinMultiplyTimes = MAX_UINT32;
+            uint32_t nBestBracketPosition = MAX_UINT32;
+            for (size_t nBracketPosition = nStartIndex; nBracketPosition < nEndIndex; ++nBracketPosition){
+                uint32_t nCurrentMultiplyTimes = matrixMultiplyTimes[nStartIndex][nBracketPosition] +
+                                                 matrixMultiplyTimes[nBracketPosition + 1][nEndIndex] +
+                                                 m_vecMatrices[nStartIndex].first * m_vecMatrices[nBracketPosition].second * m_vecMatrices[nEndIndex].second;
+                if (nCurrentMultiplyTimes < nMinMultiplyTimes){
+                    nMinMultiplyTimes = nCurrentMultiplyTimes;
+                    nBestBracketPosition = nBracketPosition;
+                }
+            }
+
+            matrixMultiplyTimes[nStartIndex][nEndIndex] = nMinMultiplyTimes;
+            matrixBracketPosition[nStartIndex][nEndIndex] = nBestBracketPosition;
+        }
+    }
+}
+
 void matrix_chain_multiplication_problem_solution_test()
 {
     print_highlight_msg(">>> Test matrix chain multiplication problem solution:\n");
-    PRINT_DEFAULT_ERROR
+    vector<pair<uint32_t, uint32_t>> vecMatrices;
+    vecMatrices.push_back(make_pair(5, 4));
+    vecMatrices.push_back(make_pair(4, 6));
+    vecMatrices.push_back(make_pair(6, 2));
+    vecMatrices.push_back(make_pair(2, 7));
+
+    vector<vector<uint32_t>> matrixMultiplyTimes;
+    vector<vector<uint32_t>> matrixBracketPosition;
+    Matrix_Chain_Multiplication_Problem_Solution oMatrixChainMultiplicationProblemSolution;
+    oMatrixChainMultiplicationProblemSolution.set_matrices(vecMatrices);
+
+    /*< test dp_bottom_up_method */
+    oMatrixChainMultiplicationProblemSolution.dp_bottom_up_method(matrixMultiplyTimes, matrixBracketPosition);
+    if ((matrixMultiplyTimes.size() >= vecMatrices.size()) &&
+        (matrixMultiplyTimes[vecMatrices.size() - 1].size() >= vecMatrices.size()) &&
+        (matrixMultiplyTimes[0][vecMatrices.size() - 1] == 158)){
+        print_correct_msg("dp_bottom_up_method OK.\n");
+    } else {
+        print_error_msg("dp_bottom_up_method Error.\n");
+    }
 }
+
--- matrix_chain_multiplication_problem.h
+++ matrix_chain_multiplication_problem.h
@@ -27,6 +27,35 @@
  *          Hu & Shing:           http://www.cs.ust.hk/mjg_lib/bibs/DPSu/DPSu.Files/0211028.pdf
  *                                http://www.cs.ust.hk/mjg_lib/bibs/DPSu/DPSu.Files/0213017.pdf
  */
+class Matrix_Chain_Multiplication_Problem_Solution
+{
+public:
+    /**
+     * @brief default constructor
+     */
+    Matrix_Chain_Multiplication_Problem_Solution();
+
+    /**
+     * @brief set matrices for the problem
+     * @param vector<pair<uint32_t, uint32_t>> &vecMatrices, matrices
+     * @return void
+     * @remarks
+     */
+    void set_matrices(vector<pair<uint32_t, uint32_t>> &vecMatrices);
+
+    /**
+     * @brief solve problem with dynamic programming bottom up method
+     * @param vector<vector<uint32_t>> &matrixMultiplyTimes, multiply times result
+     * @param vector<vector<uint32_t>> &matrixBracketPosition, multiply best bracket position
+     * @return void
+     * @remarks
+     */
+    void dp_bottom_up_method(vector<vector<uint32_t>> &matrixMultiplyTimes, vector<vector<uint32_t>> &matrixBracketPosition);
+
+private:
+    /*< matrices with <rows, columns> */
+    vector<pair<uint32_t, uint32_t>> m_vecMatrices;
+};
 
 /**
  * @brief test matrix chain multiplication problem solution
