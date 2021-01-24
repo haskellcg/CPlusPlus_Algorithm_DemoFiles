f8fc0e277c28dda999bae85e3c40ec6bac03498e
Haskellcg <haskellcg@foxmail.com>
2021-01-24 15:38:41 +0800

    add part II
    ok

--- matrix_chain_multiplication_problem.h
+++ matrix_chain_multiplication_problem.h
@@ -66,7 +66,8 @@
  *                                A polygon having .Va adjacent to Vz and V3 by sides will be called a basic polygon.
  *
  *                                THEOREM 2. A necessary but not sufficient condition for Vz-V3 to exist in an optimum partition of a basic polygon **FORMULA**
- *                                           Furthermore, if V2- V3 is not present in the l-optimum partition, then V1, V4 are always connected in the l-optimum partition.
+ *                                           Furthermore, if V2- V3 is not present in the l-optimum partition, then V1, V4 are always connected in the l-optimum
+ *                                           partition.
  *
  *                                LEMMA 5. In an optimum partition of an n-gon, let Vx, Vy, Vz and Vw be four vertices of an inscribed quadrilateral V and Vz are
  *                                         not adjacent in the quadrilateral). A necessary condition for Vx-V to exist is **FORMULA**
@@ -114,6 +115,65 @@
  *                                    until the nth vertex has been pushed onto the stack.
  *                                (b) If there are more than three vertices on the stack, add Vt-1-V to $, pop Vt off the stack and repeat this step,
  *                                    else stop
+ *
+ *                                [PART II]
+ *
+ *                                THEOREM 1. For every choice of V1,V2,(as prescribed in PartI), if the weights of the vertices of the n-gon satisfy the
+ *                                           following condition
+ *                                           **FORMULA**
+ *                                           for some k, 3 <-k _<-_ n, then every optimum partition of the n-gon contains the k-gon V1- VE Vk.
+ *
+ *                                THEOREM 2. For every choice of V1, V2, (as prescribed in Part I), if the weights of the vertices satisfy the condition
+ *                                           **FORMULA**
+ *                                           then V1- V2 and V1- V3 exist in every optimum partition of the n-gon
+ *
+ *                                THEOREM 3. Let Vx and Vz be two arbitrary vertices which are not adfacent in a polygon, and Vw be the smallest vertex
+ *                                           from Vx to Vz in the clockwise manner Vw Vx, Vw Vz), and Vy be the smallest vertex from Vz to Vx in the
+ *                                           clockwise manner Vy V, Vy V). This is shown in Fig. 1. Assume that V < Vz and Vy < Vw. The necessary condition
+ *                                           for V V to exist as an h-arc in any optimum partition is
+ *                                           **FORMULA**
+ *
+ *                                A partition is called a fan it is consists of only v-arcs joining the smallest vertex to all other vertices in the polygon.
+ *
+ *                                We shall denote the fan of a polygon V- Vb- V V by Fan (W[Wb, Wc,’’’, W). The smallest vertex V1 is called the center of the fan.
+ *
+ *                                We define a vertex as a local maximum vertex if it is larger than its two neighbors and define a vertex as a local
+ *                                minimum vertex if it is smaller than its two neighbors.
+ *
+ *                                A polygon is called a monotone polygon if there exist only one local maximum and one local minimum vertex.
+ *
+ *                                LEMMA 1. If none of the potential h-arcs appears in the 1-optimum partition of the n-gon, the l-optimum partition
+ *                                         must be the fan of the n-gon
+ *
+ *                                LEMMA 2. For any two arcs in P, say V- V. and Vp- Vq, we must have either Vi- V. above Vo Vo or Vo- Vq above Vi- V..
+ *                                         Let P be the set of all potential h-arcs in a monotone basic n-gon. P can have at most n- 3 arcs.
+ *
+ *                                LEMMA 3. Any subpolygon which is bounded by two potential h-arcs of the monotone basic n-gon is itself a monotone polygon.
+ *
+ *                                LEMMA 4. Any potential h-arc of a subpolygon bounded above and below by two potential h-arcs of the monotone basic n-gon
+ *                                         is also a potential h-arc of the monotone basic n-gon.
+ *
+ *                                We can now summarize what we have discussed. If there is no h-arc in the l-optimum partition of a monotone basic n-gon,
+ *                                the l-optimum partition must be a fan. Otherwise, the h-arcs in the l-optimum partition are all layered, one above another.
+ *                                If we consider the local maximum vertex V, and the local minimum vertex V1 as two degenerated h-arcs, then the l-optimum
+ *                                partition of a monotone basic n-gon will contain one or more monotone subpolygons, each bounded above and below by two h-arcs
+ *                                and the/-optimum partition of each of these monotone subpolygons is a fan. Then, in finding the l-optimum partition of a
+ *                                monotone basic polygon, we have only to consider those partitions which contain one or more potential h-arcs and each of the
+ *                                subpolygons between two potential h-arcs is partitioned by a fan
+ *
+ *                                An h-arc Vi Vk which divides a polygon into two subpolygons is called a positive arc with respect to the polygon if
+ *                                condition (4) is satisfied, Otherwise, it is called a negative arc with respect to the polygon.
+ *
+ *                                When an n-gon is divided into subpolygons, an h-arc is defined as positive in a subpolygon if the cost of partition of the
+ *                                subpolygon with the h-arc as the only h-arc is less than the fan in the subpolygon.
+ *
+ *                                THEOREM 4. If an h-arc is positive with respect to a polygon then the arc is positive with respect to any subpolygon containing
+ *                                           that arc. If an h-arc is positive with respect to a subpolygon, it may or may not be positive with respect to a
+ *                                           larger polygon which contains the subpolygon.
+ *
+ *                                This shows that the existence of an h-arc depends on the existence of another h-arc.
+ *
+ *                                The left-hand side of (7) is denoted by S(hk\hn)
  */
 class Matrix_Chain_Multiplication_Problem_Solution
 {
